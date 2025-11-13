a7f7b07405aef054ff4158b395ee56b843ee6b4e
vanessa <vane.busato9@gmail.com>
2025-11-13 13:27:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const button = document.getElementById("bottone-attivato"); //collega bottone html con js
+const img = document.getElementById("immagine-lampadina"); // collega imamgine html con js
+
+
+
+button.addEventListener("click", function () {
+    console.log("Bottone cliccato", img.src); //messaggio in console quando il bottone viene cliccato
+
+    if (img.src.includes("white_lamp.png")) {
+        img.src = "./img/yellow_lamp.png";
+        button.innerText = "Spegni lampadina"
+    } else {
+        img.src = "./img/white_lamp.png";
+        button.innerText = "Accendi lampadina"
+    }
+
+
+});
